c97a84ea9f2e564e90df1a807a9ab0adc45b969c
Deepesh Naini <deepesh.naini@goibibo.com>
2015-10-06 22:46:30 +0530

    initial commit

---
+++ server/boot/root.js
@@ -0,0 +1,9 @@
+var path = require('path');
+module.exports = function(server) {
+  // Install a `/` route that returns server status
+  var router = server.loopback.Router();
+  router.get('/', function (req, resp, next) {
+      resp.sendFile(path.resolve(__dirname, '../../client/index.html'));
+  });
+  server.use(router);
+};
